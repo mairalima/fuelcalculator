600d561570bdcad689c969d31ead5a7593985878
mairalima <maijucelima@gmail.com>
2025-01-13 12:46:13 -0300

    Criação de Layout

---
+++ app/src/main/java/com/example/fuelcalculator/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.fuelcalculator
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val btnIniciar = findViewById<Button>(R.id.btn_iniciar)
+
+        btnIniciar.setOnClickListener {
+            println("Vinicius")
+
+
+            val intent = Intent(this, PrecoActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
+
---
+++ app/src/main/java/com/example/fuelcalculator/PrecoActivity.kt
@@ -0,0 +1,32 @@
+package com.example.fuelcalculator
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+class PrecoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_preco)
+        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+          //  val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            //v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            //insets
+
+        val edtPreco = findViewById<TextInputEditText>(R.id.edt_preco)
+
+        val btnProximo = findViewById<Button>(R.id.btn_proximo)
+
+            btnProximo.setOnClickListener{
+                val preco= edtPreco.text
+
+                println("Maira"+preco)
+
+        }
+    }
+}
