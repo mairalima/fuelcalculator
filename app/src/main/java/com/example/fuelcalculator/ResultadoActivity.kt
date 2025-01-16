cb185b689643b4625e3fd51db93c000bd4f38837
mairalima <maijucelima@gmail.com>
2025-01-16 14:23:24 -0300

    criado telas do sistema

--- app/src/main/java/com/example/fuelcalculator/ResultadoActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/ResultadoActivity.kt
@@ -12,9 +12,9 @@ class ResultadoActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_resultado)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            //val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            //v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            //insets
         }
     }
 }
